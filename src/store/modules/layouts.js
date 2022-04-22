6363df5ce125c01b32e91b53a3ed6d83b77a2844
ljt <847129673@qq.com>
2022-04-22 20:20:18 +0800

    1. 权限处理

--- src/store/modules/layouts.js
+++ src/store/modules/layouts.js
@@ -22,8 +22,6 @@ const menuRecursion = (arr = [], treeNum = 0, ) => {
 
 
 
-
-
 export default {
 	namespaced: true,
 	state: {
@@ -31,47 +29,51 @@ export default {
 	},
 	getters: {
 		getMenuList(state) {
-			let arrMenu = []; // '/goods', '/goods/clothing', '/goods/jacket'
-			// let arrMenu02 = []; // '/goods', '/goods/clothing', '/goods/jacket'
+			let openKeysArr = []; // '/goods', '/goods/clothing', '/goods/jacket'
+			let pathAllPermission = []; // '/goods', '/goods/clothing', '/goods/jacket'
 
-			const menuRecursionFor = (arr = [], fatherItem) => {
+			const menuRecursionFor = (arr = [], fatherItem, copyMenuList02 = []) => {
 
 				for (let i = 0; i < arr.length; i++) {
 					let item = arr[i];
+					let itemCopy = copyMenuList02[i]; // 使用同一个循环 递归出所有path
+
+					if (itemCopy.auth != 0) {
+						pathAllPermission.push(itemCopy.path)
+					}
+
+					// console.log(itemCopy,   "1递归菜单7");
 
 					if (fatherItem) {
-						item.path = fatherItem.path + ',' + item.path
-					} else {
-
+						item.path = fatherItem.path + ',' + item.path;
 					}
 					if (item.children) {
-
 						// console.log(item, i, "1递归菜单6");
-
-
-						menuRecursionFor(item.children, item)
+						menuRecursionFor(item.children, item, itemCopy.children)
 					} else {
 						if (fatherItem) {
 							item.pathKeyStr = fatherItem.path
+
 							item.openKeys = fatherItem.path.split(',')
+							// console.log(item,  "1递归菜单7");
 						}
-						arrMenu.push(item);
+						openKeysArr.push(item);
 						// console.log(item, i, "1递归菜单7");
 					}
 					// console.log(item, "递归菜单2");
 				}
 			};
 
+			let copyMenuList = JSON.parse(JSON.stringify(state.menuList));
+			let copyMenuList02 = JSON.parse(JSON.stringify(state.menuList));
 
-			let arr = menuRecursionFor(JSON.parse(JSON.stringify(state.menuList)));
+			menuRecursionFor(copyMenuList, null, copyMenuList02);
 
-			// let arr02 = menuRecursion(state.menuList);
-
-			// console.log(arr, "递归菜单1");
-			console.log(arrMenu, "递归菜单3");
+			console.log(openKeysArr, state.menuList, pathAllPermission, "递归菜单3");
 			return {
-				menuList: state.menuList,
-				openKeys: arrMenu
+				menuList: state.menuList, // 左侧菜单
+				openKeys: openKeysArr, // 路由展开
+				pathAllPermission: pathAllPermission // 所有权限
 			};
 		},
 	},
