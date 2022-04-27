f91c0840800dd428fdb2723410f407edadee6e69
ljt <847129673@qq.com>
2022-04-27 15:18:14 +0800

    优化

--- src/main.js
+++ src/main.js
@@ -1,26 +1,31 @@
-import { createApp } from 'vue'
+import {
+	createApp
+} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-const app =  createApp(App)
+const app = createApp(App);
 
+import {
+	antArr
+} from "@/components/AntDesignVue.js";
 // 引入css
 // import 'ant-design-vue/dist/antd.css';
-import { antArr } from "@/components/AntDesignVue.js";
-for (const item of antArr) {
-  app.use(item);
-}
-// import { DatePicker, Input, Button } from "ant-design-vue";
-
-// import Antd from 'ant-design-vue'
-// app.use(Antd)
-// app.use(Button)
-
 
 // 引入mockjs
 require('./mock/mock.js')
 
+async function bootstrap() {
+
+	// ant-design-vue
+	for (const item of antArr) {
+		app.use(item);
+	}
 
 
+	app.use(store);
+	app.use(router);
+	app.mount('#app');
+}
 
-app.use(store).use(router).mount('#app')
+bootstrap();
